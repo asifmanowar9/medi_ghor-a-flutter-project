ea799307ff83889242e2f5ebaa6650433dd44ee6
asifmanowar9 <raiyanasif939@gmail.com>
2024-10-09 23:20:12 +0600

    13th day

---
+++ lib/data/repositories/user/user_repository.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:medi_ghor/features/personalization/models/user_model.dart';
+import 'package:medi_ghor/utils/exceptions/format_exceptions.dart';
+import 'package:medi_ghor/utils/exceptions/platform_exceptions.dart';
+
+import '../../../utils/exceptions/firebase_exceptions.dart';
+
+class UserRepository extends GetxController {
+  static UserRepository get instance => Get.find();
+
+  ///variables
+  final FirebaseFirestore _db= FirebaseFirestore.instance;
+
+
+  ///register
+  Future<void> saveUserRecord (UserModel user) async {
+    try{
+       await _db.collection('User').doc(user.id).set(user.toJson());
+    } on FirebaseException catch(e){
+      throw RFirebaseException(e.code).message;
+    }on FormatException catch(_){
+      throw RFormatException();
+    }on PlatformException catch(e){
+      throw RPlatformException(e.code).message;
+    }catch(e){
+      throw 'Something went wrong, Please try again ';
+    }
+
+  }
+
+
+
+
+}
--- lib/features/authentication/controlllers/signup/signup_controller.dart
+++ lib/features/authentication/controlllers/signup/signup_controller.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:medi_ghor/data/repositories/authentication/authentication_repository.dart';
+import 'package:medi_ghor/data/repositories/user/user_repository.dart';
+import 'package:medi_ghor/features/authentication/screens/signup/verify_email.dart';
 import 'package:medi_ghor/utils/constants/image_strings.dart';
 import 'package:medi_ghor/utils/popups/full_screen_loader.dart';
 import 'package:medi_ghor/utils/popups/loaders.dart';
 
 import '../../../../utils/helpers/network_manager.dart';
+import '../../../personalization/models/user_model.dart';
 
-
-class SignupController extends GetxController{
+class SignupController extends GetxController {
   static SignupController get instance => Get.find();
 
   ///variables
@@ -23,41 +25,65 @@ class SignupController extends GetxController{
   final password = TextEditingController();
   GlobalKey<FormState> signupFormKey = GlobalKey<FormState>();
 
-    void signup() async {
-    try{
+  void signup() async {
+    try {
       ///start loading
-      RFullScreenLoader.openLoadingDialog('We are processing your information...', RImages.staticSuccessIllustration);
+      RFullScreenLoader.openLoadingDialog(
+          'We are processing your information...',
+          RImages.staticSuccessIllustration);
 
       ///check internet connectivity
       final isConnected = await NetworkManager.instance.isConnected();
-      if(!isConnected) {
-       // RFullScreenLoader.stopLoading();
+      if (!isConnected) {
+        RFullScreenLoader.stopLoading();
         return;
       }
+
       ///form validation
-      if(!signupFormKey.currentState!.validate()){
-       // RFullScreenLoader.stopLoading();
+      if (!signupFormKey.currentState!.validate()) {
+        RFullScreenLoader.stopLoading();
         return;
       }
+
       ///privacyPolicy check
-      if(!privacyPolicy.value){
-        RLoaders.warningSnackBar(title: "Accept privacy policy.",
-          message: 'In order to create account, you must read & accept the Privacy Policy & Terms of Use',
+      if (!privacyPolicy.value) {
+        RLoaders.warningSnackBar(
+          title: "Accept privacy policy.",
+          message:
+              'In order to create account, you must read & accept the Privacy Policy & Terms of Use',
         );
         return;
       }
 
       ///register the user and authentication and save the data using firebase
-       final user = await AuthenticationRepository.instance.registerWithEmailAndPassword(email.text.trim(), password.text.trim());
+      final userCredential = await AuthenticationRepository.instance
+          .registerWithEmailAndPassword(
+              email.text.trim(), password.text.trim());
 
-    }
-    catch(e){
-      RLoaders.errorSnackBar(title: 'Ooohhhh Snap!', message: e.toString());
+      final newUser = UserModel(
+        id: userCredential.user!.uid,
+        username: userName.text.trim(),
+        email: email.text.trim(),
+        firstName: firstName.text.trim(),
+        lastName: lastName.text.trim(),
+        phoneNumber: phoneNumber.text.trim(),
+        profilePicture: '',
+      );
+      final userRepository = Get.put(UserRepository());
+      await userRepository.saveUserRecord(newUser);
 
-    }
-    finally{
       RFullScreenLoader.stopLoading();
+
+      ///show success msg
+      RLoaders.successSnackBar(
+          title: 'Congratulation',
+          message: 'Your account has been created! Verify email to continue');
+
+      Get.to(() => VerifyEmailScreen());
+    } catch (e) {
+      RFullScreenLoader.stopLoading();
+
+      RLoaders.errorSnackBar(title: 'Ohh Snap!', message: e.toString());
     }
   }
-
 }
