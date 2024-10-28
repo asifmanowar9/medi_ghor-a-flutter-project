6742a605ab123259c80fe07fe2181152f1d2a742
asifmanowar9 <raiyanasif939@gmail.com>
2024-10-28 20:26:27 +0600

    18th day

--- lib/data/repositories/authentication/authentication_repository.dart
+++ lib/data/repositories/authentication/authentication_repository.dart
@@ -26,7 +26,7 @@ class AuthenticationRepository extends GetxController {
     screenRedirect();
   }
 
-  ///function to relevant screen
+  ///function to relevant screen....
   screenRedirect() async {
     final user = _auth.currentUser;
 
